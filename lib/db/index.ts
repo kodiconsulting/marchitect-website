0235cd353a1cdb69d85d9d0e5d52475d05f6a25a
Mike Nowotarski <mike@kodiconsulting.com>
2026-04-07 14:39:23 -0600

    Use connection pooler for Supabase (disable prepared statements)

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,6 @@ import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!, { ssl: 'require' })
+const client = postgres(process.env.DATABASE_URL!, { ssl: 'require', prepare: false })
 export const db = drizzle(client, { schema })
 export type DB = typeof db
