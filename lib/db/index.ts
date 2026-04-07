2174ac80bb766f7addcc3ab2a41115d91ac0a41e
Mike Nowotarski <mike@kodiconsulting.com>
2026-04-07 13:23:51 -0600

    Fix SSL connection for Supabase

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,6 +2,6 @@ import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!)
+const client = postgres(process.env.DATABASE_URL!, { ssl: 'require' })
 export const db = drizzle(client, { schema })
 export type DB = typeof db
